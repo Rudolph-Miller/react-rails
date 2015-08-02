b5891ebc3ab5c73d9a68000ec455769ab4218201
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-08-03 00:24:31 +0900

    auto-correct.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def index
     @data = [
-      { author: "From server", description: "Data from server." }
+      { author: 'From server', description: 'Data from server.' }
     ]
     respond_to do |format|
       format.html
