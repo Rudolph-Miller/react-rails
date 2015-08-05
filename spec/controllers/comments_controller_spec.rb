babb9a9e231778abf935a4a0855ad5af13590e13
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-08-05 09:47:58 +0900

    add more tests.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe CommentsController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it 'binds @data' do
+      get :index
+      expect(assigns(:data)).to be_a(Array)
+    end
   end
 end
